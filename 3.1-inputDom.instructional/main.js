c1affd33e399237ebe6adff90800d4f510c43c9f
unknown <iam@ericruthig.com>
2021-03-18 19:14:20 -0500

    new

--- 3.1-inputDom.instructional/main.js
+++ 3.1-inputDom.instructional/main.js
@@ -1,12 +1,34 @@
 /* 
-    - Using the html and js files provided, write a webpage that asks the user for input on a store, a quantity, and a product.  
+    - Using the html and js files provided, write a webpage that asks the user for input on a 
+    store, a quantity, and a product.  
     
     - Save the user input in 3 separate variables. 
-        - Once the user has input their information, your webpage should console.log a sentence using those 3 variables. Using dom manipulation, you should also display the user input to the screen. For example: 'I shopped at *Target* for *5* *apples*' may be output to the console & screen. 
+        - Once the user has input their information, your webpage should console.log a 
+        sentence using those 3 variables. Using dom manipulation, you should also display 
+        the user input to the screen. For example: 'I shopped at *Target* for *5* *apples*'
+         may be output to the console & screen. 
         
     - Do not alter the HTML doc.
 
-    - This does not need to be styled in any sort of fashion, but feel free to if you would like!
+    - This does not need to be styled in any sort of fashion, but feel free to if you would
+     like!
 */
 
-//NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+//NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL 
+// IN CANVAS
+
+
+let store = document.getElementById('store');
+let product = document.getElementById('product');
+let quantity = document.getElementById('quantity');
+
+function setVar() {
+    console.log(`I went to ${store.value} and bought ${quantity.value} ${product.value}!`);
+};
+
+setVar();
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', setVar()); {
+    console.log('received')
+};
